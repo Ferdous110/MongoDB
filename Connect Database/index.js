e26e9cb3a4b775afe541c22b79782e60bf8933e7
ANJUM FERDOUS <anjumferdous110@gmail.com>
2025-02-03 20:03:41 +0600

    Product send mongoos

--- Connect Database/index.js
+++ Connect Database/index.js
@@ -3,11 +3,24 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = 3002;
 
+app.use(express.json());  // Middleware
+app.use(express.urlencoded({extended: true}));
+
 // create product schema 
-const productsSchema = new mongoose.Schema({
-    title: String,
-    price: Number,
-    description: String,
+
+const productsSchema = new mongoose.Schema({ 
+    title: {
+        type: String,
+        required: true,
+    },
+    price:  {
+        type: Number,
+        required: true,
+    },
+    description:  {
+        type: String,
+        required: true,
+    },
     createdAt: {
         type: Date,
         default: Date.now
@@ -21,15 +34,32 @@ app.get("/", (req, res)=> {
     res.send("<h1>Welcome to home page</h1>")
 })
 
+app.post("/products", async (req, res)=> {
+   try {
+    // get data from request body
+     const newProduct = new Product({
+        title: req.body.title,
+        price:  req.body.price,
+        description: req.body.description
+     })
+     const productData = await newProduct.save();
+
+     res.status(201).send(productData);
+   } catch (error) {
+    res.status(500).send({ message: error.message });
+   }
+});
+
 // mongoose.connect('mongodb://127.0.0.1:27017/testProductDB')
 // .then(()=> console.log("DB is connected"))
 // .catch((error) => {
 //     console.log("DB is not connected")
 //     console.log(error);
 //     process.exit(1);
-// }) 
+// })  
 
 // another away connected mongoose use async and await
+
 const connectDB = async () => {
     try {
       await  mongoose.connect('mongodb://127.0.0.1:27017/testProductDB');
@@ -43,9 +73,10 @@ const connectDB = async () => {
 
 
 app.listen(PORT, async ()=>{ 
-    console.log(`Server is runing at http://localhost:${PORT}`);
+    console.log(` Server is runing at http://localhost:${PORT} `);
     await connectDB();
-})
+});
+
 
 // NoSQl 
-// DATABASE => collections  => document 
+// DATABASE => collections => document 
