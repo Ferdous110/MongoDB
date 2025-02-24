8d42d29e9c0ef66582f106a8a1470363129d8d99
ANJUM FERDOUS <anjumferdous110@gmail.com>
2025-02-25 00:09:28 +0600

    Register and connected MongoBD

--- passport/app.js
+++ passport/app.js
@@ -1,13 +1,36 @@
+// bcrypt password 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+// passport && session 
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require('connect-mongo');
+
 
 const express = require("express");
 const cors = require("cors");
 const ejs = require("ejs");
 const app = express();
+require("./config/database")
+const User = require("./models/user.models")
+require("dotenv").config();
 
 app.set("view engine", "ejs");
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONG_URL,
+    collectionName: "sessions";
+  }),
+ // cookie: { secure: true }
+}))
 
 //  base url 
 app.get("/", (req, res)=> {
@@ -15,9 +38,19 @@ app.get("/", (req, res)=> {
 })
 
 // register : post 
-app.post("/register", (req, res)=> {
+app.post("/register", async (req, res)=> {
     try {
-        res.status(201).send("User is created");
+        const user = await User.findOne({username: req.body.username});
+        if(user) return res.status(400).send("user allready exists");
+        bcrypt.hash(req.body.password, saltRounds, async (err, hash)=> {
+            const newUser = new User({
+                username: req.body.username,
+                password: hash
+            });
+        await newUser.save();
+        res.status(201).redirect("/login");
+        });
+       
     } catch (error) {
         res.status(500).send(error.message);
     }
